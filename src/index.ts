9425c2ee7f953112f2fa88dcef17202686c11108
ayaf-bless <blessambel1@gmail.com>
2022-01-12 00:15:17 +0300

    setting up apollo server and file structure

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-const age: number = 44;
+import { ApolloServer } from "apollo-server";
+import { typeDefs } from "./schema";
+import { Query } from "./resolvers";
 
-console.log(age);
+const server = new ApolloServer({ resolvers: { Query }, typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`server running on ${url}`);
+});
